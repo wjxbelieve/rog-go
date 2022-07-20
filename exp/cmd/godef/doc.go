998a45b33051d7399485eb7ffe50dd90fa5b9946
wjxbelieve <wjxbelieve@hotmail.com>
2022-07-20 22:58:23 +0800

    after
    init   after

---
+++ exp/cmd/godef/doc.go
@@ -0,0 +1,13 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+
+Godef prints the source location of definitions in Go programs.
+
+This is no longer the canonical place for the godef
+source - it has now moved to github.com/rogpeppe/godef.
+
+*/
+package main
